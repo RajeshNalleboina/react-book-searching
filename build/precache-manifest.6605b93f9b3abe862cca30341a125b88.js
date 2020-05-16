228a40b28d24127ca840f5f36f6db52679e70005
Rajesh2545 <rajeshnalleboina94@gmail.com>
2020-05-16 08:16:11 +0530

    my reads app

---
+++ build/precache-manifest.6605b93f9b3abe862cca30341a125b88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c93d6bed476a4f81162",
+    "url": "/react-book-searching/static/css/main.43119070.chunk.css"
+  },
+  {
+    "revision": "8c93d6bed476a4f81162",
+    "url": "/react-book-searching/static/js/main.8c93d6be.chunk.js"
+  },
+  {
+    "revision": "db7c91d224f748548891",
+    "url": "/react-book-searching/static/js/1.db7c91d2.chunk.js"
+  },
+  {
+    "revision": "2330114ed85a20ca37f4",
+    "url": "/react-book-searching/static/js/runtime~main.2330114e.js"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/react-book-searching/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/react-book-searching/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/react-book-searching/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3ad0170a26881384825f7c181975c539",
+    "url": "/react-book-searching/index.html"
+  }
+];
